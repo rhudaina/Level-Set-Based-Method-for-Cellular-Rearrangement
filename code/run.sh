d8ff9537fc595a8962fb4afbc2e80e4d370e66d1
RHU <54294106+rhudaina@users.noreply.github.com>
2021-06-23 22:33:41 +0800

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -7,5 +7,6 @@ mkdir ./output
 echo -e "Running proposed level set-based scheme..."
 gcc -Wall functions.c main.c -g -lfftw3 -lm -o cell.out
 ./cell.out ../data/parameters.txt ../data/initconfig.txt
+matlab -nodisplay -r "addpath(genpath('code')); generate_result"
 
 echo -e "Done!\n"
